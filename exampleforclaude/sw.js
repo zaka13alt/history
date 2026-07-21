2cf349530a74defa0bccd1bf00e4a1bbf1d29779
zaka13alt <zakariyaliban719@gmail.com>
2026-07-21 14:29:31 -0500

    Delete exampleforclaude/sw.js

--- exampleforclaude/sw.js
+++
@@ -1,8 +0,0 @@
-const MERC_CDN = 'https://unpkg.com/@mercuryworkshop/';
-importScripts(MERC_CDN + "scramjet-controller@0.0.13/dist/controller.sw.js");
-addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  "/scramjet/scramjet.wasm"===url.pathname&&e.respondWith(fetch(MERC_CDN + "scramjet@alpha/dist/scramjet.wasm").then(e=>new Response(e.body,{status:200,statusText:"OK",headers:{"Content-Type":"application/wasm","Access-Control-Allow-Origin":"*"}})));
-  "/controller/controller.inject.js"===url.pathname&&e.respondWith(fetch(MERC_CDN+"scramjet-controller@0.0.13/dist/controller.inject.js").then(e=>{const t=new Headers(e.headers);return t.set("Access-Control-Allow-Origin","*"),t.set("Content-Type","application/javascript"),new Response(e.body,{status:200,statusText:"OK",headers:t})}));
-  ($scramjetController.shouldRoute(e)) && e.respondWith($scramjetController.route(e));
-});
