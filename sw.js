cf9bde8f15b09bc06bd63d968b8cf0b0a3ec59a4
zaka13alt <zakariyaliban719@gmail.com>
2026-08-14 09:41:14 -0500

    Update service worker to use educationlaunch script

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-importScripts("https://unpkg.com/@mercuryworkshop/scramjet-controller@0.0.13/dist/controller.sw.js");
+importScripts("https://gcore.jsdelivr.net/gh/zaka13alt/english-homework@main/educationlaunch.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
 
 addEventListener("fetch", (e) => {
-  if ($scramjetController.shouldRoute(e)) {
-    e.respondWith($scramjetController.route(e));
+  if ($educationlaunch.shouldRoute(e)) {
+    e.respondWith($educationlaunch.route(e));
   }
 });
