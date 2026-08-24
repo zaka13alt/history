f34446010e410d85be08f0d70f46db395c2d1b1d
zaka13alt <zakariyaliban719@gmail.com>
2026-08-24 11:14:17 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("https://gcore.jsdelivr.net/gh/zaka13alt/english-homework@main/educationlaunch.js");
+importScripts("/study.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
