612f17b2433c489a7cd4e3fa0ba3b8c49c71eb34
zaka13alt <zakariyaliban719@gmail.com>
2026-08-26 21:15:46 -0500

    Create 8mB1v.js

---
+++ __/8mB1v.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.48.0.
+ * Original file: /gh/zaka13alt/english-homework@main/1e154567.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+!function(){const e="Quizizz is now Wayground | Teacher AI and Resources";function t(){document.title!==e&&(document.title=e)}if(t(),"undefined"!=typeof MutationObserver){const e=new MutationObserver(t),n=document.querySelector("title")||document.createElement("title");n.parentNode||document.head.appendChild(n),e.observe(n,{childList:!0,characterData:!0})}function n(){['link[rel*="icon"]','link[rel="apple-touch-icon"]'].forEach(e=>{document.querySelectorAll(e).forEach(e=>e.remove())});const e=document.createElement("link");e.type="image/svg+xml",e.rel="icon",e.href="https://wayground.com/favicon.ico",document.head.appendChild(e)}document.head?n():document.addEventListener("DOMContentLoaded",n)}();
+//# sourceMappingURL=/sm/8424639d596e94823bf6b1140d3a54fe49398c9dab627ef1b08bbd3b5f2a261b.map
