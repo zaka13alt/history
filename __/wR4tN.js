659ffdcf3721d73bfcbaca5531dde4a54f07292e
zaka13alt <zakariyaliban719@gmail.com>
2026-08-26 21:15:04 -0500

    Create wR4tN.js

---
+++ __/wR4tN.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.48.0.
+ * Original file: /gh/zaka13alt/english-homework@main/de/german-learning.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+window.addEventListener("beforeunload",e=>{e.preventDefault(),e.returnValue=""});
+//# sourceMappingURL=/sm/df247093cbc3f9b3bffbf9eb71b582ed930239d9b1acb6a81ff0eba64f4f7b6f.map
