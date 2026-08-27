cafeaced5f445692687bf10c2d020055401a9cec
zaka13alt <zakariyaliban719@gmail.com>
2026-08-26 21:14:27 -0500

    Create 2jG7q.js

---
+++ __/2jG7q.js
@@ -0,0 +1,8 @@
+/**
+ * Minified by jsDelivr using Terser v5.48.0.
+ * Original file: /gh/zaka13alt/english-homework@main/fra/learn.chunk.107.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
+(()=>{"use strict";const e=[];function o(o){return function(...r){e.push({type:o,args:r,timestamp:Date.now()})}}const r=["log","info","warn","error","debug","trace","dir","dirxml","table","group","groupCollapsed","groupEnd","time","timeEnd","timeLog","count","countReset","assert","clear","profile","profileEnd","timeStamp"];for(const e of r)Object.defineProperty(console,e,{value:o(e),writable:!1,configurable:!1,enumerable:!0});Object.defineProperty(console,"__history__",{value:e,writable:!1,configurable:!1,enumerable:!1}),Object.freeze(console)})();
+//# sourceMappingURL=/sm/7e84b74da7bf4c5a2bf9b598d54c7d45409b6e0a30b068e2134390282140b7ab.map
